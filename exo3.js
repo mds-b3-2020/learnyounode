77d1c61098a2c30b7ae38c48f717dd10808d01d5
Mathieu TUDISCO <oss@mathieutu.dev>
2020-09-29 15:57:44 +0200

    Update and rename exo3 to exo3.js

--- exo3.js
+++ exo3.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+
 const [node, path, filePath] = process.argv;
+
 const content = fs.readFileSync(filePath, 'utf8');
 const lines = content.split('\n');
-console.log(lines.length-1);
+
+console.log(lines.length - 1);
